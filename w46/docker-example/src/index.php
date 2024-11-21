8d8f3748ce795db4641904eed8fa7a365b1b3cb5
Gabriel Kereklidis Paulin <gabriel.paulin111@gmail.com>
2024-11-21 09:51:18 +0100

    save

--- w46/docker-example/src/index.php
+++ w46/docker-example/src/index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hey hey!";
+$servername = "mariadb";
+$username = "mariadb";
+$password = "mariadb";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=mariadb", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
